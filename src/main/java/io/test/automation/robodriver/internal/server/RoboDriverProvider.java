0d79560bf9e74c1fdfb5c36cfb2d8eb6122f0d41
Gerald Ehmayer <gerald.ehmayer@gmail.com>
2017-09-24 11:21:12 +0200

    Fix start robodriver + Selenium server.

--- src/main/java/io/test/automation/robodriver/internal/server/RoboDriverProvider.java
+++ src/main/java/io/test/automation/robodriver/internal/server/RoboDriverProvider.java
@@ -1,14 +1,24 @@
 package io.test.automation.robodriver.internal.server;
 
+import java.util.logging.Logger;
+
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.server.DriverProvider;
 
 import io.test.automation.robodriver.RoboDriver;
+import io.test.automation.robodriver.RoboDriverCommandExecutor;
+import io.test.automation.robodriver.internal.LoggerUtil;
 
 public class RoboDriverProvider implements DriverProvider {
+	
+	private static Logger LOGGER = LoggerUtil.get(RoboDriverCommandExecutor.class);
 
+	public RoboDriverProvider() {
+		LOGGER.info(()->String.format("%s created.", RoboDriverProvider.class.getSimpleName()));
+	}
+	
 	@Override
 	public Capabilities getProvidedCapabilities() {
 		return RoboDriver.getDesiredCapabilities();
--- start_seleniumserver.cmd
+++ start_seleniumserver.cmd
@@ -1,10 +1,12 @@
 @echo off
 
-echo java executable path:         %1
+echo java executable path:               %1
 
-echo selenium server jar path:     %2
+echo selenium server jar path:           %2
 
-echo gecko driver executable path: %3
+echo chrome driver executable path:      %3
 
-start %1 -Dwebdriver.gecko.driver=%3 -cp ./target/classes;%2 org.openqa.grid.selenium.GridLauncherV3 -port 4444
+echo robodriver class path or .jar file: %4
+
+start %1 -Dwebdriver.chrome.driver=%3 -cp %4;%2 org.openqa.grid.selenium.GridLauncherV3 -port 4444
 
